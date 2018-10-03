132bfd16682d7e359129c2bf720b2427c577d711
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-03 03:06:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   def self.find_or_create_by_name(name)
     if song = self.find_by_name(name) 
       song
-    else
+    else self.create_by_name(name)
   end
   
   def self.alphabetical
