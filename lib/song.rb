65c1c61b012a874698cf20f7c7579703884bdebf
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-03 02:59:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ class Song
   end
   
   def self.create_by_name(name)
-    self.new_by_name(name).tap{ |song| song.name.save }
+    self.new_by_name(name).tap{ |song| song.save }
   end
   
   def self.find_by_name(name)
