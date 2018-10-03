c369bdb1f36f498b8d0d9c58a133c10db6ad2143
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-03 02:29:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end
 
   def self.all
     @@all
@@ -9,5 +14,46 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    #@@create = create
+    #song = Song.create
+  end
+  
+  def self.new_by_name
+    #@@new_by_name = new_by_name
+    #song = Song.new_by_name
+  end
+  
+  def self.create_by_name
+    @@create_by_name
+  end
+  
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name }
+  end
+  
+  def self.find_or_create_by_name
+    @@find_or_create_by_name
+  end
+  
+  def self.alphabetical
+    sorted = @@all.sort_by { |song| song.name }
+    return sorted
+  end
+  
+  def self.new_from_filename
+    @@new_from_filename
+  end
+  
+  def self.create_by_filename
+    @@create_by_filename
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 end
