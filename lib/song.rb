e799392f96bae5d6103a949e7cd018051328a295
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-03 03:13:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   end
   
   def self.new_from_filename(file)
-    file.split("-")
+    song, artist = file.split(" - ")
     
     #song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
   end
