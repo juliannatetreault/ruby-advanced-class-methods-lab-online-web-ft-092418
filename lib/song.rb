c52977e7a34405b4283d751f6ecd61f794b5f926
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-03 02:46:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
-  def initialize(name, artist_name)
-    @name = name
-    @artist_name = artist_name
-  end
+  #def initialize(name, artist_name)
+  #  @name = name
+  #  @artist_name = artist_name
+  #end
 
   def self.all
     @@all
